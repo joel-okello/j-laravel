274868446a781777fc45c17d2512c89371595edb
joel-okello <okellojoelacaye@gmail.com>
2018-06-26 14:15:19 +0300

    adding script to app.blade

--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -82,5 +82,7 @@
             @yield('content')
         </div>
     </div>
+    
+
 </body>
 </html>
